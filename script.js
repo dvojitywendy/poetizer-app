90a0e59f6eefb8b45e47f066e10b4c848b20d588
Ludmila Kellerová <dvojitywendy@seznam.cz>
2022-02-26 19:31:35 +0100

    #2 Implement NEXT button fixed

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-var baseUrl = "https://api.poetizer.com";
-var webUrl = "https://poetizer.com";
-var device_token = window.localStorage.getItem("device_token");
-var limit, offset;
+const BASE_URL = 'https://api.poetizer.com';
+const WEB_URL = 'https://poetizer.com';
+let deviceToken = window.localStorage.getItem('device_token');
+let limit, offset;
+let calledAlready = false;
 
-function showUserInfo(user_id) {
+function showUserInfo(userId) {
 
-    fetch(baseUrl + '/users/' + user_id, {
+    fetch(`${BASE_URL}/users/${userId}`, {
         method: 'get',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Device-token ' + device_token
+            'Authorization': `Device-token ${deviceToken}`
         }
     })
         .then(response => response.json())
         .then(user => {
             document.getElementById('id01').style.display = 'none';
 
-            var img = document.createElement("img");
-            img.src = user.picture;
+            const userImg = document.createElement('img');
+            userImg.src = user.picture;
 
-            var name = document.createElement("p");
-            var textName = document.createTextNode("Jste přihlášen pod účtem: " + user.name);
-            name.appendChild(textName);
+            const userNamePgf = document.createElement('p');
+            userNamePgf.innerText = `Jste přihlášen pod účtem: ${user.name}`;
 
-            document.getElementById('login-area').innerHTML = img.outerHTML;
-            document.getElementById('login-area').innerHTML += name.outerHTML;
+            document.getElementById('login-area').innerHTML = userImg.outerHTML;
+            document.getElementById('login-area').innerHTML += userNamePgf.outerHTML;
         });
 }
 
 function login() {
-    var email = document.getElementById("uname").value;
-    var password = document.getElementById("psw").value;
-    var params = { "email": email, "password": password, "platform": "android", "device_name": "Randomdroid 9" };
+    const email = document.getElementById('uname').value;
+    const password = document.getElementById('psw').value;
+    const params = { 'email': email, 'password': password, 'platform': 'android', 'device_name': 'Randomdroid 9' };
 
     getToken(params);
 }
 
 function getToken(params) {
-    fetch(baseUrl + '/devices/email', {
+    fetch(`${BASE_URL}/devices/email`, {
         method: 'post',
         headers: {
             'Content-Type': 'application/json',
@@ -45,11 +45,10 @@ function getToken(params) {
         body: JSON.stringify(params)
     })
         .then(response => response.json())
-        .then(token_info => {
-            device_token = token_info.device_token;
-            window.localStorage.setItem("device_token", device_token);
-            user_id = token_info.user_id;
-            showUserInfo(user_id);
+        .then(tokenInfo => {
+            deviceToken = tokenInfo.device_token;
+            window.localStorage.setItem('device_token', deviceToken);
+            showUserInfo(tokenInfo.user_id);
         });
 
 }
@@ -58,82 +57,96 @@ function getFirstPoemsByTags() {
     getPoemsByTags(6, 0);
 }
 
-// This is quick&dirty solution when undefined limit and offset returns all poems for certain tag
 function getPoemsByTags(limit, offset) {
-    var querytags = document.getElementById("tags").value;
-    var params = { "tags": [querytags] };
+    const queryTags = document.getElementById('tags').value;
+    const params = { 'tags': [queryTags] };
 
-    fetch(baseUrl + '/poems/search?limit=' + limit + '&offset=' + offset, {
+    fetch(`${BASE_URL}/poems/search?limit=${limit}&offset=${offset}`, {
         method: 'post',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Device-token ' + device_token
+            'Authorization': `Device-token ${deviceToken}`
         },
         body: JSON.stringify(params)
     })
         .then(response => response.json())
         .then(poems => {
-            var maindiv = document.getElementById("main");
-            maindiv.innerHTML = "";
+            const mainDiv = document.getElementById('main');
+            if (!calledAlready) {
+                mainDiv.innerHTML = '';
+            }
 
             poems.poems.forEach(element => {
-                var article = document.createElement("article");
+                const article = document.createElement('article');
 
-                var linkTitle = document.createElement("a");
-                linkTitle.href = `${webUrl}/poem/${element.id}`;
-                linkTitle.target = "_blank";
-                var title = document.createElement("h2");
-                var textTitle = document.createTextNode(element.title);
-                title.appendChild(textTitle);
-                linkTitle.appendChild(title);
+                const poemTitleLnk = document.createElement('a');
+                poemTitleLnk.href = `${WEB_URL}/poem/${element.id}`;
+                poemTitleLnk.target = '_blank';
+                const poemTitleHdr = document.createElement('h2');
+                poemTitleHdr.innerText = element.title;
+                poemTitleLnk.appendChild(poemTitleHdr);
 
-                var para = document.createElement("p");
-                para.insertAdjacentHTML('afterBegin', element.text);
+                const poemBodyPgf = document.createElement('p');
+                poemBodyPgf.insertAdjacentHTML('afterBegin', element.text);
 
-                var linkAuthor = document.createElement("a");
-                linkAuthor.href = `${webUrl}/author/${element.author.id}`;
-                linkAuthor.target = "_blank";
-                linkAuthor.innerText = element.author.name;
+                const authorLnk = document.createElement('a');
+                authorLnk.href = `${WEB_URL}/author/${element.author.id}`;
+                authorLnk.target = '_blank';
+                authorLnk.innerText = element.author.name;
 
-                var hashtags = document.createElement("p");
-                hashtags.innerText = `Tags: ${element.tags}`;
+                const hashtagsPgf = document.createElement('p');
+                hashtagsPgf.innerText = `Tags: ${element.tags}`;
 
-                article.appendChild(linkTitle);
-                article.appendChild(para);
-                article.insertAdjacentHTML('beforeend', `Autor: ${linkAuthor.outerHTML}`);
-                article.appendChild(hashtags);
+                article.appendChild(poemTitleLnk);
+                article.appendChild(poemBodyPgf);
+                article.insertAdjacentHTML('beforeend', `Autor: ${authorLnk.outerHTML}`);
+                article.appendChild(hashtagsPgf);
 
-                maindiv.appendChild(article);
+                mainDiv.appendChild(article);
             });
-        
-            /* TODO
-                if (poems.next != null) {
-                setLimitAndOffset();
+
+            if (poems.poems.length == limit) {
+                setLimitAndOffset(poems.next);
                 createNextButton();
-                        }
-            */
+            }
+            if (poems.poems.length < limit) {
+                hideNextButton();
+            }
         })
 }
 
-function setLimitAndOffset() {
-    var next_text = poems.next;
-    const myArray = next_text.split("=");
-    const bef_limit = myArray[1].split("&");
-    limit = bef_limit[0];
-    offset = myArray[2];
+function setLimitAndOffset(nextPageLink) {
+    const offsetArray = nextPageLink.split('=');
+    const limitArray = offsetArray[1].split('&');
+    limit = limitArray[0];
+    offset = offsetArray[2];
 }
 
 function createNextButton() {
-    var next_button = document.createElement("button");
-    next_button.setAttribute('onclick', getPoemsByTags(limit, offset));
-    next_button.value = "Další básně";
-    document.getElementById("next-area").appendChild(next_button);
+    if (!calledAlready) {
+        const nextBtn = document.createElement('button');
+        nextBtn.onclick = () => getPoemsByTags(limit, offset);
+        nextBtn.setAttribute('id', 'next-poem-button');
+        nextBtn.innerText=`Další básně`;
+        document.getElementById('next-area').appendChild(nextBtn);
+    }
+    calledAlready = true;
+}
+
+function hideNextButton() {
+    const mainDiv = document.getElementById('main');
+    if (calledAlready) {
+        document.getElementById('next-poem-button').hidden = true;
+    }
+    const endOfListPgf = document.createElement('p');
+    endOfListPgf.innerText = `Žádné další básně`;
+    mainDiv.appendChild(endOfListPgf);
 }
 
 window.onload = function () {
     document.querySelector('#tags').addEventListener('keypress', function (e) {
         if (e.key === 'Enter') {
-            getPoemsByTags(limit, offset);
+            getFirstPoemsByTags();
         }
     });
     document.querySelector('#psw').addEventListener('keypress', function (e) {
